914f4d27c5f27ea02f6d32c94e9c7cc31fbc097e
Pricila <pricila.albero@gmail.com>
2020-03-24 14:50:06 +0100

    getmapping

--- src/main/java/com/example/demo/Saludo.java
+++ src/main/java/com/example/demo/Saludo.java
@@ -1,5 +1,15 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Saludo {
+    @GetMapping("/hola")
+    public String getHola(){
+
+        return "Hola, ¿que tal?";
+    }
+
 
 }
