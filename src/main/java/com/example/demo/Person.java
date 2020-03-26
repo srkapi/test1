b5835304e833973cda36994f52d9bce10a29cc43
Pricila <pricila.albero@gmail.com>
2020-03-26 08:44:55 +0100

    restcontroller

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/person")
+public class Person {
+private String name;
+private String surname;
+    public Person(){
+
+    }
+    @GetMapping
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /*@Override
+    public String toString(){
+        return "Name: "+ name;
+    }*/
+}
