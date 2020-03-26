7b86a80b76d7b910c1b88e002265015673cb0128
Your Name <you@example.com>
2020-03-26 10:50:34 +0100

    task

--- src/main/java/com/example/demo/PersonController.java
+++ src/main/java/com/example/demo/PersonController.java
@@ -1,27 +1,31 @@
 package com.example.demo;
 
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
     @PostMapping
-    public Person savePerson() {
+    public Person savePerson() { // recibir por parametro un objeto java  y devolver el mismo object
         Person person = new Person();
 
         person.setName("kapi");
         person.setSurname("iglesias");
         return person;
     }
-    @GetMapping
-    public Person getPerson(){
-        Person person = new Person();
-        person.getName();
-        person.getSurname();
-        return person;
-    }
+
+
+//    @GetMapping
+//    public Person getPerson() {
+//        Person person = new Person();
+//        person.getName();
+//        person.getSurname();
+//        return person;
+//    }
 
 
 }
