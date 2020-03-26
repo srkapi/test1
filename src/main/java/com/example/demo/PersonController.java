3f0b3b220f0cc7310387f505bf8cd0771f3a8720
Pricila <pricila.albero@gmail.com>
2020-03-26 11:16:02 +0100

    requestbody

--- src/main/java/com/example/demo/PersonController.java
+++ src/main/java/com/example/demo/PersonController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +16,11 @@ public class PersonController {
     private List<Person> personList = new ArrayList<>();
 
     @PostMapping
-    public Person savePerson() { // recibir por parametro un objeto java  y devolver el mismo object
-        Person person = new Person();
-
+    public Person savePerson(@RequestBody Person person) { // recibir por parametro un objeto java  y devolver el mismo object
+        //Person person = new Person();
+/*
         person.setName("kapi");
-        person.setSurname("iglesias");
+        person.setSurname("iglesias");*/
         return person;
     }
 
