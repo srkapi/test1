98ada904ba6e2534d8b80c5df187c48b509e1968
Pricila <pricila.albero@gmail.com>
2020-04-29 14:43:12 +0200

    mapper models

--- src/main/java/com/example/demo/adapter/out/persistence/mapper/MapperPersistence.java
+++ src/main/java/com/example/demo/adapter/out/persistence/mapper/MapperPersistence.java
@@ -1,5 +1,34 @@
 package com.example.demo.adapter.out.persistence.mapper;
 
+import com.example.demo.adapter.out.persistence.model.PersonajeModel;
+import com.example.demo.application.domain.Personaje;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
 public class MapperPersistence {
-    
+    //pasa de modelo de dominio a modelo de bbdd
+    public PersonajeModel toModelPersistence(Personaje domain){
+        return PersonajeModel.builder()
+                .nombre(domain.getNombre())
+                .turno(domain.getTurno())
+                .salaSalida(domain.getSalaSalida())
+                .build();
+    }
+
+    //pasa de modelo de bbdd a modelo de dominio
+    public Optional toDomain(PersonajeModel model){
+        if (model !=null){
+            Personaje build = Personaje.builder().marca(model.getMarca())
+                    .nombre(model.getNombre())
+                    .turno(model.getTurno())
+                    .salaSalida(model.getSalaSalida())
+                    .build();
+            return Optional.of(build);
+        }else{
+            return Optional.empty();
+        }
+
+    }
 }
